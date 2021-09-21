4cf0c89a140cf3071f3ab7103feab2f2c7a67e85
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 01:53:44 +0200

    feat: created home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import HeaderTabs from '../components/header/HeaderTabs'
+
+export default function Home() {
+    return (
+        <View>
+            <HeaderTabs/>
+        </View>
+    )
+}

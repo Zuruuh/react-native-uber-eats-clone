2a7a516c4e7bac54e4dac060b0e1010a591d353d
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 01:52:19 +0200

    feat: created babel config file feat: created imports routes shortcuts

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            components: "./src/components",
+            features: "./src/features",
+            screens: "./src/screens",
+            store: "./src/app",
+          },
+        },
+      ],
+    ],
   };
 };

1a6c528dc3b877143f65f27b98dd864044506f54
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 01:55:22 +0200

    feat: added redux provider

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Provider } from "react-redux";
+import { SafeAreaView } from "react-native";
+import { store } from "./src/app/store";
+import Home from "./src/screens/Home";
+import SafeViewAndroid from "./src/components/views/SafeViewAndroid";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+        <Home />
+      </SafeAreaView>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

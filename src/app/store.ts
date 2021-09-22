fc20166ef3b6a3e4344d8ecfc4fa8c4186c85501
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 15:13:53 +0200

    feat: added reducers for categories and bottom popper

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { tabReducer } from "features/tabs/tabSlice";
+import { categoryReducer } from "features/categories/categorySlice";
+import { bottomPopperReducer } from "features/bottomPopper/bottomPopperSlice";
 
 export const store = configureStore({
   reducer: {
     tab: tabReducer,
+    category: categoryReducer,
+    bottomPopper: bottomPopperReducer,
   },
 });
 

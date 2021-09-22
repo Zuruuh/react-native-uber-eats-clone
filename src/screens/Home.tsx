ab74028f1f705e1fc627d7acd5c708c945132c3a
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 02:36:07 +0200

    feat: displayed PlacesSearchBar component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { View } from 'react-native'
-import HeaderTabs from '../components/header/HeaderTabs'
+import PlacesSearchBar from "components/search/PlacesSearchBar";
+import React from "react";
+import { View } from "react-native";
+import HeaderTabs from "components/header/HeaderTabs";
 
 export default function Home() {
-    return (
-        <View>
-            <HeaderTabs/>
-        </View>
-    )
+  return (
+    <View style={{ backgroundColor: "white", padding: 15 }}>
+      <HeaderTabs />
+      <PlacesSearchBar />
+    </View>
+  );
 }

31c7bb310290c8efdc509d7b2d46d7a5673ad6f5
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 15:18:34 +0200

    feat: created category component feat: set activeCategory to clicked category on button press

---
+++ src/components/category/Category.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ImageURISource,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "store/store";
+import { setCategory } from "features/categories/categorySlice";
+import { setbottomPopper } from "features/bottomPopper/bottomPopperSlice";
+
+export default function Category(props: { title: string; image: string }) {
+  const activeCategory = useSelector(
+    (state: RootState) => state.category.value
+  );
+  activeCategory;
+  const dispatch = useDispatch();
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        dispatch(setbottomPopper(""));
+        dispatch(setCategory(props.title));
+      }}
+    >
+      <View
+        style={{
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "#eee",
+            paddingVertical: 5,
+            paddingHorizontal: 10,
+            borderRadius: 10,
+          }}
+        >
+          <Image
+            source={props.image as ImageURISource}
+            style={{
+              width: 65,
+              height: 55,
+              resizeMode: "contain",
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 13,
+            fontWeight: "900",
+          }}
+        >
+          {props.title}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}

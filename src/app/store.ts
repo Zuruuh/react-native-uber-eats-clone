63629f87b5940b9f42e6ec3b413e96a4a4dce400
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 20:52:37 +0200

    feat: added navbarReducer to global store

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,6 +3,7 @@ import { tabReducer } from "reducers/tabs/tabSlice";
 import { categoryReducer } from "reducers/categories/categorySlice";
 import { bottomPopperReducer } from "reducers/bottomPopper/bottomPopperSlice";
 import { searchBarReducer } from "reducers/searchBar/searchBarSlice";
+import { navbarReducer } from "reducers/navbar/navbarSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ export const store = configureStore({
     category: categoryReducer,
     bottomPopper: bottomPopperReducer,
     searchBar: searchBarReducer,
+    navbar: navbarReducer,
   },
 });
 

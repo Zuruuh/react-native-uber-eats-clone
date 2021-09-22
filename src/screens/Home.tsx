a8734b8b2c824830eb0b7ecdc062697e74f46828
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 20:18:34 +0200

    feat: Added restaurants Items to Home Screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,11 @@
 import PlacesSearchBar from "components/search/PlacesSearchBar";
 import React from "react";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import HeaderTabs from "components/header/HeaderTabs";
 import Categories from "components/category/Categories";
 import CategoryPopper from "components/category/CategoryPopper";
 import PopperContainer from "components/popper/PopperContainer";
+import RestaurantItems from "components/restaurant-items/RestaurantItems";
 
 export default function Home() {
   return (
@@ -12,12 +13,14 @@ export default function Home() {
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
         <PlacesSearchBar />
-        <Categories />
       </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems />
+      </ScrollView>
       <PopperContainer>
         <CategoryPopper />
       </PopperContainer>
     </>
   );
 }
-// 55 minutes

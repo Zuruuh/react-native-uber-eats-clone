89472e24b211efdf86344560930bdb6018d338a9
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-23 00:25:58 +0200

    feat: created menu data file

---
+++ data/menu.ts
@@ -0,0 +1,52 @@
+import { Menu } from "types/menu";
+
+export const menu = [
+  {
+    title: "Tenderstem galette",
+    description:
+      "This open-face style tart is packed with fresh spring veg and plenty of goat’s cheese.",
+    price: 16.3,
+    image:
+      "https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/07/658230-1-eng-GB_tenderstem-and-spring-onion-galettes-with-goats-cheese-768x960.jpg",
+  },
+  {
+    title: "Tarragon chicken",
+    description:
+      "A quick, herby tarragon chicken recipe – it serves two, and is bound to satisfy for midweek meal.",
+    price: 11.4,
+    image:
+      "https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/08/443099-1-eng-GB_tarragon-chicken-768x1026.jpg",
+  },
+  {
+    title: "Mozzarella and pesto toasties",
+    description:
+      "The ultimate toasted sandwich, oozing with mozzarella and pesto and topped with crispy shards of parma ham",
+    price: 12.3,
+    image:
+      "https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/07/886158-1-eng-GB_cheese-on-toast-1-768x960.jpg",
+  },
+  {
+    title: "Tenderstem galette",
+    description:
+      "This open-face style tart is packed with fresh spring veg and plenty of goat’s cheese.",
+    price: 16.3,
+    image:
+      "https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/07/658230-1-eng-GB_tenderstem-and-spring-onion-galettes-with-goats-cheese-768x960.jpg",
+  },
+  {
+    title: "Tarragon chicken",
+    description:
+      "A quick, herby tarragon chicken recipe – it serves two, and is bound to satisfy for midweek meal.",
+    price: 11.4,
+    image:
+      "https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/08/443099-1-eng-GB_tarragon-chicken-768x1026.jpg",
+  },
+  {
+    title: "Mozzarella and pesto toasties",
+    description:
+      "The ultimate toasted sandwich, oozing with mozzarella and pesto and topped with crispy shards of parma ham",
+    price: 12.3,
+    image:
+      "https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/07/886158-1-eng-GB_cheese-on-toast-1-768x960.jpg",
+  },
+] as Menu[];

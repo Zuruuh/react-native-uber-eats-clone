b407616cf4d9b1ae6747471157ed0b5e8fc6bd02
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 21:02:48 +0200

    feat: modified require paths

--- data/categories.ts
+++ data/categories.ts
@@ -2,31 +2,31 @@ export const categories = [
   {
     id: "4bf58dd8d48988d1e0931735",
     name: "Coffee & Tea",
-    image: require("../assets/images/coffee.png"),
+    image: require("assets/images/coffee.png"),
   },
   {
     id: "4bf58dd8d48988d1e0931734",
     name: "Soft Drinks",
-    image: require("../assets/images/soft-drink.png"),
+    image: require("assets/images/soft-drink.png"),
   },
   {
     id: "4bf58dd8d48988d1e0931733",
     name: "Bakery Items",
-    image: require("../assets/images/bread.png"),
+    image: require("assets/images/bread.png"),
   },
   {
     id: "4bf58dd8d48988d1e0931732",
     name: "Fast Foods",
-    image: require("../assets/images/fast-food.png"),
+    image: require("assets/images/fast-food.png"),
   },
   {
     id: "4bf58dd8d48988d1e0931731",
     name: "Pick-up",
-    image: require("../assets/images/shopping-bag.png"),
+    image: require("assets/images/shopping-bag.png"),
   },
   {
     id: "4bf58dd8d48988d1e0931730",
     name: "Deals",
-    image: require("../assets/images/deals.png"),
+    image: require("assets/images/deals.png"),
   },
 ];

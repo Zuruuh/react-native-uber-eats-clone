cd1391242691c2fc311a1410e043f4f776d6d86f
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 15:17:57 +0200

    feat: created categories component feat: embedded multiple Category components inside

---
+++ src/components/category/Categories.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { View } from "react-native";
+import { useDispatch } from "react-redux";
+import Category from "./Category";
+import SeeAllCategories from "./SeeAllCategories";
+import { categories as categoriesData } from "data/categories";
+import { Category as CategoriesType } from "types/category";
+
+export default function Categories() {
+  const [categories, setCategories] = React.useState([]);
+  React.useEffect(() => {
+    const getCategories = () => {
+      setCategories(
+        categoriesData.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        ) as CategoriesType[]
+      );
+    };
+    getCategories();
+  }, []);
+  const stripCategories = (length) => {
+    return categories.slice(0, length);
+  };
+  return (
+    <View
+      style={{
+        marginTop: 5,
+        backgroundColor: "#fff",
+        paddingVertical: 10,
+        justifyContent: "space-around",
+        flexDirection: "row",
+      }}
+    >
+      {categories.length > 4 &&
+        stripCategories(3).map((category) => (
+          <Category
+            key={category.id}
+            title={category.name}
+            image={category.image}
+          />
+        ))}
+      {categories.length > 4 && <SeeAllCategories />}
+      {categories.length < 5 &&
+        categories.map((category) => (
+          <Category
+            key={category.id}
+            title={category.name}
+            image={category.url}
+          />
+        ))}
+    </View>
+  );
+}

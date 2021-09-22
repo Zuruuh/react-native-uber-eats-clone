c0b4462555e850d5ebca6fcbd5cd98b665afee6c
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 20:54:59 +0200

    feat: moved savefiew provider to screens that needs it instead of app root

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { SafeAreaView } from "react-native";
 import { store } from "./src/app/store";
 import Home from "./src/screens/Home";
-import SafeViewAndroid from "./src/components/views/SafeViewAndroid";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaView
-        style={[
-          SafeViewAndroid.AndroidSafeArea,
-          { backgroundColor: "#eee", flex: 1 },
-        ]}
-      >
-        <Home />
-      </SafeAreaView>
+      <Home />
     </Provider>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,22 @@
 import PlacesSearchBar from "components/search/PlacesSearchBar";
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 import HeaderTabs from "components/header/HeaderTabs";
 import Categories from "components/category/Categories";
 import CategoryPopper from "components/category/CategoryPopper";
 import PopperContainer from "components/popper/PopperContainer";
 import RestaurantItems from "components/restaurant-items/RestaurantItems";
+import Nav from "components/navbar/Nav";
+import SafeViewAndroid from "components/views/SafeViewAndroid";
 
 export default function Home() {
   return (
-    <>
+    <SafeAreaView
+      style={[
+        SafeViewAndroid.AndroidSafeArea,
+        { backgroundColor: "#eee", flex: 1 },
+      ]}
+    >
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
         <PlacesSearchBar />
@@ -18,9 +25,10 @@ export default function Home() {
         <Categories />
         <RestaurantItems />
       </ScrollView>
+      <Nav />
       <PopperContainer>
         <CategoryPopper />
       </PopperContainer>
-    </>
+    </SafeAreaView>
   );
 }

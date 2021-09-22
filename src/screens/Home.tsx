aa94b4271592c311bac80510b6ed3c4c4d9ac392
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-22 15:14:28 +0200

    feat: added popper container in home view note: might move it to app for it to be usable on all screens

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,12 +2,22 @@ import PlacesSearchBar from "components/search/PlacesSearchBar";
 import React from "react";
 import { View } from "react-native";
 import HeaderTabs from "components/header/HeaderTabs";
+import Categories from "components/category/Categories";
+import CategoryPopper from "components/category/CategoryPopper";
+import PopperContainer from "components/popper/PopperContainer";
 
 export default function Home() {
   return (
-    <View style={{ backgroundColor: "white", padding: 15 }}>
-      <HeaderTabs />
-      <PlacesSearchBar />
-    </View>
+    <>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <PlacesSearchBar />
+        <Categories />
+      </View>
+      <PopperContainer>
+        <CategoryPopper />
+      </PopperContainer>
+    </>
   );
 }
+// 55 minutes

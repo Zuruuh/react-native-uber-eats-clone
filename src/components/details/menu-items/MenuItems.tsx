08b915b1ff46dfb017398c0841d03f3339eae801
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-23 13:44:38 +0200

    feat: moved styles to stylesheet

--- src/components/details/menu-items/MenuItems.tsx
+++ src/components/details/menu-items/MenuItems.tsx
@@ -1,30 +1,62 @@
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import {
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { menu } from "data/menu";
 import MenuItem from "./MenuItem";
 import { Divider } from "react-native-elements";
+import ViewCart from "components/details/cart/ViewCart";
 
-export default function MenuItems() {
+export default function MenuItems(props: { nav: any; route: any }) {
+  const [scrolled, setScrolled] = React.useState(true);
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const e = event.nativeEvent;
+    let max = e.layoutMeasurement.height / 6;
+    // setScrolled(e.contentOffset.y > (max / 100) * 5);
+    // TODO Add bounce/spring animation when button is visible
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      marginHorizontal: 10,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "700",
+    },
+    divider: {
+      marginBottom: 10,
+    },
+    blank: {
+      height: 30,
+    },
+    buttonContainer: {
+      position: "absolute",
+      bottom: scrolled ? 150 : 0,
+      left: 0,
+      width: "100%",
+    },
+  });
   return (
-    <View
-      style={{
-        marginHorizontal: 10,
-      }}
-    >
-      <Text style={{ fontSize: 24, fontWeight: "700" }}>Menu</Text>
-      <ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Menu</Text>
+      <ScrollView onScroll={(event) => handleScroll(event)}>
         {menu.map((item, index) => (
           <View key={`${index}-${item.title}`}>
             <MenuItem item={item} />
-            <Divider width={0.5} style={{ marginBottom: 10 }} />
+            <Divider width={0.5} style={styles.divider} />
           </View>
         ))}
-        <View
-          style={{
-            height: 30,
-          }}
-        ></View>
+        <View style={styles.blank}></View>
       </ScrollView>
+      <View style={styles.buttonContainer}>
+        <ViewCart nav={props.nav} route={props.route} />
+      </View>
     </View>
   );
 }

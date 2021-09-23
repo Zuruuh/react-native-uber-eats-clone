c0ec07bb237af6480d16c69103282f9536d79863
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-23 15:30:08 +0200

    feat: added firstLaunch verification note: might move code to navigation.tsx file

--- App.tsx
+++ App.tsx
@@ -2,11 +2,31 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "store/store";
 import RootNavigation from "./navigation";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <RootNavigation />
-    </Provider>
-  );
+  const [firstLaunch, setFirstLaunch] = React.useState(null);
+  React.useEffect(() => {
+    AsyncStorage.getItem("alreadyLaunched").then((value) => {
+      if (value === null) {
+        AsyncStorage.setItem("alreadyLaunched", "true");
+        setFirstLaunch(true);
+      } else {
+        setFirstLaunch(false);
+      }
+    });
+  }, []);
+  if (firstLaunch === null) {
+    return null;
+  } else if (firstLaunch === true) {
+    return (
+      <Provider store={store}>
+        <RootNavigation />
+      </Provider>
+    );
+  } else {
+    // return (
+    //   <Login />
+    // )
+  }
 }

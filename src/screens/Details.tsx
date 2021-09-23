d078a05b3b4f9436ae1e3d929acf6060c3b52e50
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-23 13:45:55 +0200

    feat: passed route to MenuItems

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -4,14 +4,14 @@ import { Divider } from "react-native-elements";
 import About from "components/details/about/About";
 import MenuItems from "components/details/menu-items/MenuItems";
 
-export default function Details(props: { route: any }) {
+export default function Details(props: { route: any; nav: any }) {
   return (
     <>
       <View style={{ height: "100%" }}>
         <About route={props.route} />
         <Divider width={1.8} style={{ marginTop: 20, marginBottom: 10 }} />
         <View style={{ flex: 1 }}>
-          <MenuItems />
+          <MenuItems nav={props.nav} route={props.route} />
         </View>
       </View>
     </>

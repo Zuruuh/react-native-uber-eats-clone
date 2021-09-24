7722c6a7a8ae0018954e696dc20502e4cf6c07a3
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-24 23:02:47 +0200

    feat: added dark mode toggle button

--- src/components/account/UserActions.tsx
+++ src/components/account/UserActions.tsx
@@ -1,11 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSignOutAlt, faSun } from "@fortawesome/free-solid-svg-icons";
 import Button from "components/global/Button";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import { useDispatch } from "react-redux";
 import { logOut } from "reducers/user/userSlice";
 import { setNavbar } from "reducers/navbar/navbarSlice";
+import { useDarkMode } from "react-native-dynamic";
+import palette from "styles/palette";
+import { toggle } from "reducers/mode/displayModeSlice";
 
 interface props {
   nav: any;
@@ -13,6 +16,7 @@ interface props {
 
 export default function UserActions({ nav }: props) {
   const dispatch = useDispatch();
+  const darkMode = useDarkMode();
   const logout = () => {
     try {
       AsyncStorage.removeItem("user").then(() => {
@@ -23,9 +27,20 @@ export default function UserActions({ nav }: props) {
     } catch (err) {}
   };
 
+  const toggleDarkMode = () => {
+    dispatch(toggle());
+  };
+
   return (
     <View style={styles.container}>
       <Button text="Log Out" onPress={logout} icon={faSignOutAlt} />
+      <Button
+        text="Toggle Dark Mode"
+        onPress={toggleDarkMode}
+        icon={darkMode ? faSun : faMoon}
+        color={darkMode ? palette.darkPrimary : palette.lightPrimary}
+        background={darkMode ? palette.lightPrimary : palette.darkPrimary}
+      />
     </View>
   );
 }

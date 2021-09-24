7697bae6d4e3e53776bd9ee1443f0ae1c4bc87c0
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-24 19:18:00 +0200

    feat: removed useless styling

--- src/components/global/styles/Button.ts
+++ src/components/global/styles/Button.ts
@@ -2,19 +2,18 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   button: {
-    width: "100%",
+    minWidth: "100%",
     marginTop: 10,
     padding: 10,
     borderRadius: 5,
-    flexDirection: "row",
-    justifyContent: "center",
+    alignItems: "center",
     position: "relative",
     minHeight: 15,
   },
   text: {
     fontSize: 18,
     fontWeight: "bold",
-    textAlign: "center",
+    // textAlign: "center",
   },
   iconWrapper: {
     width: 48,
@@ -25,16 +24,4 @@ export default StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  icon: {},
-  // iconWrapper: {
-  //   width: 30,
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   fontWeight: "bold",
-  // },
-  // textWrapper: {
-  //   flex: 1,
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  // },
 });

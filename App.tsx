255636c24bef8428db4cc6a4b6daadb3874b5af3
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-24 04:57:33 +0200

    feat: moved script to navigation file

--- App.tsx
+++ App.tsx
@@ -2,31 +2,11 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "store/store";
 import RootNavigation from "./navigation";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  const [firstLaunch, setFirstLaunch] = React.useState(null);
-  React.useEffect(() => {
-    AsyncStorage.getItem("alreadyLaunched").then((value) => {
-      if (value === null) {
-        AsyncStorage.setItem("alreadyLaunched", "true");
-        setFirstLaunch(true);
-      } else {
-        setFirstLaunch(false);
-      }
-    });
-  }, []);
-  if (firstLaunch === null) {
-    return null;
-  } else if (firstLaunch === true) {
-    return (
-      <Provider store={store}>
-        <RootNavigation />
-      </Provider>
-    );
-  } else {
-    // return (
-    //   <Login />
-    // )
-  }
+  return (
+    <Provider store={store}>
+      <RootNavigation />
+    </Provider>
+  );
 }

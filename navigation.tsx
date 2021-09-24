ad50c92e9583fe06acbf52fcdb6b2e4aedeb1cee
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-24 04:57:47 +0200

    feat: added login, register, and account routes

--- navigation.tsx
+++ navigation.tsx
@@ -7,19 +7,39 @@ import { NavigationContainer } from "@react-navigation/native";
 import Home from "screens/Home";
 import Details from "screens/Details";
 import Onboarding from "screens/Onboarding";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Login from "screens/Login";
+import Register from "screens/Register";
+import Account from "screens/Account";
 
 export default function RootNavigation() {
   const Stack = createStackNavigator();
+  const [firstLaunch, setFirstLaunch] = React.useState(true);
   const screenOptions = {
     headerShown: false,
   } as StackNavigationOptions;
 
+  React.useEffect(() => {
+    AsyncStorage.getItem("alreadyLaunched").then((value) => {
+      if (!value) {
+        AsyncStorage.setItem("alreadyLaunched", "true");
+        setFirstLaunch(true);
+      }
+    });
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Navigator
+        initialRouteName={firstLaunch ? "login" : "login"}
+        screenOptions={screenOptions}
+      >
         <Stack.Screen name="onboarding" component={Onboarding} />
-        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="register" component={Register} />
+        <Stack.Screen name="account" component={Account} />
         <Stack.Screen name="details" component={Details} />
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );

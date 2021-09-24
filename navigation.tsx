c18a1034665bd242084f0a8be6d5a3031d6f52f6
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-24 19:15:29 +0200

    feat: added Address and AddressCreation screens

--- navigation.tsx
+++ navigation.tsx
@@ -11,6 +11,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import Login from "screens/Login";
 import Register from "screens/Register";
 import Account from "screens/Account";
+import Address from "screens/Address";
+import AddressCreate from "screens/AddressCreate";
 
 export default function RootNavigation() {
   const Stack = createStackNavigator();
@@ -31,13 +33,15 @@ export default function RootNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName={firstLaunch ? "login" : "login"}
+        initialRouteName={firstLaunch ? "login" : "onboarding"}
         screenOptions={screenOptions}
       >
         <Stack.Screen name="onboarding" component={Onboarding} />
         <Stack.Screen name="register" component={Register} />
         <Stack.Screen name="account" component={Account} />
         <Stack.Screen name="details" component={Details} />
+        <Stack.Screen name="address" component={Address} />
+        <Stack.Screen name="addresscreate" component={AddressCreate} />
         <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="home" component={Home} />
       </Stack.Navigator>
